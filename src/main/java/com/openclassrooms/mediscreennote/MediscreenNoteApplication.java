eeb42e454b5a74d9662754996e9bc4f0003a5a56
hallier <ahallier@isara.fr>
2022-02-01 16:28:34 +0100

    Ci : database connexion name database in application.properties create a document to feed database

--- src/main/java/com/openclassrooms/mediscreennote/MediscreenNoteApplication.java
+++ src/main/java/com/openclassrooms/mediscreennote/MediscreenNoteApplication.java
@@ -1,13 +1,29 @@
 package com.openclassrooms.mediscreennote;
 
+import com.openclassrooms.mediscreennote.model.entity.NoteEntity;
+import com.openclassrooms.mediscreennote.model.repository.NoteRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 public class MediscreenNoteApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MediscreenNoteApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner runner(NoteRepository noteRepository) {
+    return args -> {
+      NoteEntity noteEntity = new NoteEntity(7, List.of("diabete type 2", "pancreatite"));
+      noteRepository.insert(noteEntity);
+    };
   }
 
 }
